f19b7b810ca3273bbb25c6d1a6eb8fdfbd4b8977
leoboyerbx <leo.boyer@outlook.com>
2020-05-04 12:47:25 +0200

    WIP on gradient

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 import countdown from 'countdown'
 import CountdownNumber from './modules/CountdownNumber'
+import Granim from 'granim'
 
 const $ = document.querySelector.bind(document)
 
@@ -9,7 +10,7 @@ let data =  {
   day: 11
 }
 fetch('https://gist.githubusercontent.com/leoboyerbx/06ab5c985abcf78b27345b9acea59498/raw/decomptefinement_data.json').then(res => res.json()).then(res => {
-  data = res
+data = res
 })
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -17,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const hoursNumber = new CountdownNumber($('#hours-number'), 24)
   const minutesNumber = new CountdownNumber($('#minutes-number'), 60)
   const secondsNumber = new CountdownNumber($('#seconds-number'), 60)
-
+  
   function onUpdateCounter (callback) {
     const timespan = countdown(null, new Date(data.year, data.month, data.day), countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS)
     const indexes = ['days', 'hours', 'minutes', 'seconds']
@@ -31,13 +32,45 @@ document.addEventListener('DOMContentLoaded', () => {
     minutesNumber.setValue(timespan.minutes)
     secondsNumber.setValue(timespan.seconds)
   }
-
+  
   setInterval(onUpdateCounter, 1000)
-
-  const hero = $('#hero')
-  const currentHour = new Date().getHours()
-  const gradientClass = currentHour < 5 ? 'night' : currentHour < 8 ? 'before-morning' : currentHour < 12 ? 'morning' : currentHour < 18 ? 'afternoon' : currentHour < 21 ? 'evening' : 'night'
-  hero.classList.add('gradient-' + gradientClass)
-
+  
+  // const hero = $('#hero')
+  // const currentHour = new Date().getHours()
+  // const gradientClass = currentHour < 5 ? 'night' : currentHour < 8 ? 'before-morning' : currentHour < 12 ? 'morning' : currentHour < 18 ? 'afternoon' : currentHour < 21 ? 'evening' : 'night'
+  // hero.classList.add('gradient-' + gradientClass)
+  
   onUpdateCounter()
+  
+  // Gradient background
+  const background = new Granim({
+    element: '#canvas-gradient',
+    // name: 'canvas-gradient',
+    // elToSetClassOn: '#hero',
+    direction: 'diagonal',
+    isPausedWhenNotInView: true,
+    stateTransitionSpeed: 500,
+    defaultStateName: 'before-morning',
+    states : {
+      "before-morning": {
+        gradients: [
+          ['#A43931', '#1D4350'],
+          ['#646881', '#32746D'],
+          ['#33658A', '#86BBD8']
+        ],
+        transitionSpeed: 5000
+      },
+      "violet-state": {
+        gradients: [
+          ['#9D50BB', '#6E48AA'],
+          ['#4776E6', '#8E54E9']
+        ],
+        transitionSpeed: 2000
+      },
+      "orange-state": {
+        gradients: [ ['#FF4E50', '#F9D423'] ],
+        loop: false
+      }
+    }
+  })
 })
